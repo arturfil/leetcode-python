0d117c5889c6872faca054b2025ad6311a964116
arturfil <arturfil@hotmail.com>
2022-02-19 14:48:42 -0500

    added folder and solution for two sum

---
+++ main.py
@@ -0,0 +1,11 @@
+# from Arrays import two_sum
+from Arrays.two_sum import TwoSum
+
+def main():
+    target = 9
+    nums = [2,7,11,15]
+    two = TwoSum()
+    two.twoSum(nums, target)
+
+if __name__ == "__main__":
+    main()
