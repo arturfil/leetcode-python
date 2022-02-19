af366149071f4c1304d0a2d6ef14233eab23b258
arturfil <arturfil@hotmail.com>
2022-02-19 17:18:52 -0500

    got rid of explanations

--- HashMaps/two_sum.py
+++ HashMaps/two_sum.py
@@ -10,19 +10,3 @@ class TwoSum:
             else:
                 hashmap[nums[i]] = i
 
-'''
-    TESTING
-    target = 9
-    nums = [2,7,11,15]
-    two = TwoSum()
-    two.twoSum(nums, target)
-
-    EXPLANATION
-    - You want to create a dictionary which in python would be the same as a hashmap.
-    - If you have NOT seen the number minus the target (the other number that adds up),
-      then you add the number as a key and the index as a value
-    - Once you iterate throuh the List or Array at some point, you will see the other 
-      value needed and where "if complement in hashmap" will be true, then you
-      return that current number's index and the reciprocal "hashmap[ complement ]" which 
-      will give the value of the index
-'''
