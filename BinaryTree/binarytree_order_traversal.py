6b3ace2ba491d5baed17c205a3725419b1c03343
arturfil <arturfil@hotmail.com>
2023-09-18 13:49:00 -0400

    added randomiszed problem choose and also added Binary Trees section

---
+++ BinaryTree/binarytree_order_traversal.py
@@ -0,0 +1,8 @@
+from ast import List
+from typing import Optional
+from BinaryTree.tree_node import TreeNode
+
+
+class BTOrderTraversal:
+    def level_order(self, root: Optional[TreeNode]) -> List[List[int]]:
+        pass
--- main.py
+++ main.py
@@ -2,24 +2,10 @@ from Arrays.buy_sell_stock import BuySellSock
 from Arrays.product_of_array_except_self import ProductOfArrayExceptSelf
 from Graphs.number_of_islands import NumberOfIslands
 from Matrix.matrix import SpiralMatrix
+from random_problem import chose_random
 
 def main():
-    '''
-    grid = [
-        ["1","1","1","1","0"],
-        ["1","1","0","1","0"],
-        ["1","1","0","0","0"],
-        ["0","0","0","0","0"]
-    ]
-
-    island = NumberOfIslands()
-    island.num_is_islands(grid)
-    '''
-
-    spr_matrix = SpiralMatrix()
-
-    print(spr_matrix.generate_matrix(3))
-
+   chose_random() 
 
 if __name__ == "__main__":
     main()
