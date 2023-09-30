966a36ae5a08d87840c8af30525ac98586380d78
arturfil <arturfil@hotmail.com>
2023-09-30 14:58:02 -0400

    addd merge intervals

--- Intervals/MergeIntervals.py
+++ Intervals/MergeIntervals.py
@@ -3,10 +3,10 @@ from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x: x[0]) # sort ascending by first value
-        res = []
+        res = [intervals[0]]
         
         for interval in intervals:
-            if not res or res[-1][1] < interval[0]:
+            if res[-1][1] < interval[0]:
                 res.append(interval)
             else:
                 res[-1][1] = max(res[-1][1], interval[1])
--- Strings/longest_common_subsequence.py
+++ Strings/longest_common_subsequence.py
@@ -1,14 +1,15 @@
 class LongestCommonSubsequence:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # tabulation
-        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)] 
 
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len(text2) -1, -1, -1):
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
+        len1, len2 = len(text1), len(text2)
+        dp = [[0 for j in range(len2 + 1)] for i in range(len1 + 1)] 
+
+        for i in range(1, len1 + 1):
+            for j in range(1, len2 + 1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
                 else:
-                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
-        return dp[0][0]
+        return dp[len1][len2]
 
